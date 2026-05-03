32da731a44191615cff2c9500b1e87d86c83b0ce
TIEMOUO <dorcastiemouo@gamil.com>
2026-05-03 19:55:49 +0100

    style: amelioration du fichier index.html ajout des animations fluides  commentaires et meileure organisation du code(identations)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Point d'entrée du programme Rust.
+// Cette fonction est exécutée lorsque l'application démarre.
 fn main() {
+    // Affiche une ligne de texte dans la console.
     println!("Hello, world!");
 }
